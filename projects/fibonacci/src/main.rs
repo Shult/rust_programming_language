b49cbe4423f87f413a6caece9b677266c1f042ea
Sylvain <mestres.sin@gmail.com>
2024-10-10 12:10:44 +0200

    Fibonacci suite

--- projects/fibonacci/src/main.rs
+++ projects/fibonacci/src/main.rs
@@ -1,3 +1,32 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Fibonacci suite with Rust!");
+    let mut fibonacci_sequence: Vec<i64> = Vec::new();
+
+    loop {
+        println!("Please enter the number corresponding to the position in the Fibonacci sequence you would like to know!");
+        let mut n = String::new();
+        io::stdin()
+            .read_line(&mut n)
+            .expect("Failed to read line");
+
+        let n: usize = n.trim().parse().expect("Number");
+        let mut i: usize = 2;
+        
+        fibonacci_sequence.clear();
+        fibonacci_sequence.push(0);
+        fibonacci_sequence.push(1);
+
+        while i < n {
+            fibonacci_sequence.push(fibonacci(fibonacci_sequence[i-1], fibonacci_sequence[i-2]));
+            i += 1;
+        }
+        println!("The {}th number of the fibonacci suite is {}!",n, fibonacci_sequence[n-1] )
+    }
+}
+
+
+fn fibonacci(n1: i64, n2: i64) -> i64 {
+    n1+n2
 }
---
+++ projects/fibonacci/target/debug/deps/fibonacci-0354592865d27556.d
@@ -0,0 +1,5 @@
+D:\Travail\Rust project\the_rust_programming_language\projects\fibonacci\target\debug\deps\libfibonacci-0354592865d27556.rmeta: src/main.rs
+
+D:\Travail\Rust project\the_rust_programming_language\projects\fibonacci\target\debug\deps\fibonacci-0354592865d27556.d: src/main.rs
+
+src/main.rs:
